3b4ec058f460164b8993d81dc792b1909bf0995f
Anjali-st <anjali90908080@gmail.com>
2023-08-08 03:17:47 +0530

    initial commit

---
+++ SubjectObserver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConApp_Oop_School_Data_Design_Practice_Project
+{
+    public class SubjectObserver : ITeacherObserver
+    {
+        private readonly ITeacher _teacher;
+
+        public SubjectObserver(ITeacher teacher)
+        {
+            _teacher = teacher;
+        }
+
+        public void Update(ISubject subject)
+        {
+            Console.WriteLine($"{_teacher.Name} is now teaching {subject.Name} ({subject.SubjectCode})");
+        }
+    }
+}
